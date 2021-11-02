fc929e01dec4cd1b12bc864faa3e50dadfe6b00a
Insaf <v.insaf228@gmial.com>
2021-11-02 05:44:26 +0200

    Added "switch" instead of "if"

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
+// function calc(mathAction, a, b) {
+//     if (mathAction == '+') {
+//         return (+a + +b === 0) ? 0 : +a + +b || 'Error';
+//     } else if (mathAction == '-') {
+//         return (a - b === 0) ? 0 : a - b || 'Error';
+//     } else if (mathAction == '*') {
+//         return (a * b === 0) ? 0 : a * b || 'Error'; 
+//     } else if (mathAction == '/') {
+//         return (a / b === 0) ? 0 : a / b || 'Error';
+//     } else if (mathAction == '**') {
+//         return (a ** b === 0) ? 0 : a ** b || 'Error';
+//     } else if (mathAction == '%') {
+//         return (a % b === 0) ? 0 : a % b || 'Error';
+//     } else {
+//         return 'Unknown operation';
+//     }
+// }
+
+//console.log(calc('', ,));
+
 function calc(mathAction, a, b) {
-    if (mathAction == '+') {
-        return (+a + +b) || 'Error';
-    } else if (mathAction == '-') {
-        return (a - b) || 'Error';
-    } else if (mathAction == '*') {
-        return (a * b) || 'Error'; 
-    } else if (mathAction == '/') {
-        return (a / b) || 'Error';
-    } else if (mathAction == '**') {
-        return (a ** b) || 'Error';
-    } else if (mathAction == '%') {
-        return (a % b) || 'Error';
-    } else {
-        return 'Unknown operation';
-    }
+  switch(mathAction) {
+    case '+':
+			return (+a + +b === 0) ? 0 : +a + +b || 'Error';
+			break;
+		case '-':
+			return (a - b === 0) ? 0 : a - b || 'Error';
+			break;
+		case '*':
+			return (a * b === 0) ? 0 : a * b || 'Error';
+			break;
+		case '/':
+			return (a / b === 0) ? 0 : a / b || 'Error';
+			break;
+		case '**':
+			return (a ** b === 0) ? 0 : a ** b || 'Error';
+			break;
+		case '%':
+			return (a % b === 0) ? 0 : a % b || 'Error';
+			break;
+		default:
+			return 'Unknown operation';
+	}
 }
 
-// console.log(calc('', ,));
+console.log(calc('%', 8, 3));
