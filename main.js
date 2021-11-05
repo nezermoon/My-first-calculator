593ff7f767e33a738c8bca37c55a521e29c19b5a
Insaf Valiev <76104587+nezermoon@users.noreply.github.com>
2021-11-05 13:31:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 
 function calc(a, act, b) {
 	
-	let mathAction = {
-		sum: a + b,
-		sub: a - b,
-		mult: a * b,
-		div: a / b, 
-		exp: a ** b,
-		rem: a % b,
-	};
+let mathAction = {
+	sum: a + b,
+	sub: a - b,
+	mult: a * b,
+	div: a / b, 
+	exp: a ** b,
+	rem: a % b,
+};
 
-	if (typeof(a) !== 'number' || typeof(b) !== 'number') {
-		return 'Error';
-	} else if (mathAction[act] === undefined) {
-		 return 'Unknown operation';
-	} else {
-		return mathAction[act];
+if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+	return 'Error';
+} else if (mathAction[act] === undefined) {
+	return 'Unknown operation';
+} else {
+	return mathAction[act];
 	}
 }
 
